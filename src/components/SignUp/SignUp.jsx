d56cfb21a1191b56492019a2b21dff4fb1f6bd22
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-12-08 17:44:42 +0600

    login and signup added

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,8 +3,23 @@ import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Please Sign Up</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" placeholder='Your Email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" placeholder='Your Password' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" name="confirm" id="" placeholder='Confirm Your Password' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
         </div>
     );
 };
